2db194bbf02135d5eb8bd335bdcc29095bb6d3cd
Carl <carlbrainegebe@gmail.com>
2021-09-25 10:24:06 -0500

    render todos

--- src/features/todos/TodoContainer.js
+++ src/features/todos/TodoContainer.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Todo from "./Todo"
+import { v4 as uuid } from "uuid"
 
 function TodoContainer() {
+
+  const todos = useSelector( (state) => state.todos.entities )
+  const renderTodos = todos.map( (todo) => <Todo key={uuid()} todo={todo}/> ) 
   return <div>TodoContainer</div>;
 }
 
